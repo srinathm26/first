54e0f8673e90980e8742a3dab98eaee4fd23aff0
srinathm26 <101381586+srinathm26@users.noreply.github.com>
2022-04-02 12:43:01 +0530

    Add files via upload

---
+++ 3_Implementation/atm.h
@@ -0,0 +1,18 @@
+#include<string.h>
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node {
+    char statement[50];
+    struct node* link;
+} node;
+
+void withdrawMoney(node **, int *);
+void Updateamount(node **, char *);
+void Modifyhistory(node **);
+void History(node **);
+void Generator(void);
+int validation(void);
+void Balanceamount(int *);
+void Deposit(node **, int *);
